0022fd29c4ae05f42f5776cb0c49bab700740fbe
Arsh <chhokara@gmail.com>
2021-01-09 16:13:45 -0800

    creating user model

---
+++ back-end/src/entity/user.js
@@ -0,0 +1,165 @@
+// exports user model
+module.exports = (sequelize, DataTypes) => {
+  // define user model
+  sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'first name is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'first name is required',
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'last name is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'last name is required',
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'email is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'email is required',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'password is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'password is required',
+        },
+      },
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'street is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'street is required',
+        },
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'city is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'city is required',
+        },
+      },
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'country is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'country is required',
+        },
+      },
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'phone number is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'phone number is required',
+        },
+      },
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'gender is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'gender is required',
+        },
+      },
+    },
+    photoURL: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'photoURL is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'photoURL is required',
+        },
+      },
+    },
+    isEmployer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'isEmployer is required',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'isEmployer is required',
+        },
+      },
+    },
+  });
+};
