448b134df7705d0e24885d29eae94e500efa8af5
Tawshif Alam <tawshif27@gmail.com>
2021-01-09 16:32:04 -0800

    setup sqlite db

---
+++ back-end/src/config/config.js
@@ -0,0 +1,14 @@
+export const configuration = {
+  development: {
+    dialect: 'sqlite',
+    storage: 'quick-jobs.db',
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: 'quick-jobs.db',
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: 'quick-jobs.db',
+  },
+};
---
+++ back-end/src/entity/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import Sequelize from 'sequelize';
+import { configuration } from '../config/config.js';
+import User from './user.js';
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = configuration[env];
+
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+// fs.readdirSync(__dirname)
+//   .filter((file) => {
+//     return (
+//       file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+//     );
+//   })
+//   .forEach((file) => {
+//     const model = sequelize['import'](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+db.User = User(sequelize, Sequelize.DataTypes);
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
